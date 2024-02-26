be3f804f31e664b3ffca28ea80b5a7b38694d0be
Suresh-CS <Suresh.bscsses21@iba-suk.edu.pk>
2024-02-26 15:17:53 +0500

    Done

---
+++ Practice1.js
@@ -0,0 +1,14 @@
+const car = ["Honda", "Hd", "Revo"]
+// console.log(car) 
+// car.push("KUMAR")
+// console.log(car)
+// car.pop[1]
+// console.log(car)
+
+//  console.log(car.shift())
+
+//  car.unshift("Kumar")
+//  console.log(car)
+
+//  console.log(car.slice(car))
+
